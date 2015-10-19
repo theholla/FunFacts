e75fac822388ed969017e2562e7b36c0ecceb633
the holla <holla@bikefarm.org>
2015-10-18 23:51:04 -0700

    Refactor with new FactBook class

---
+++ app/src/main/java/treehouse/funfacts/FactBook.java
@@ -0,0 +1,31 @@
+package treehouse.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by admin on 10/18/15.
+ */
+public class FactBook {
+    //member variables
+    public String[] mFacts = {
+            "Ants stretch when they wake up in the morning",
+            "Ostriches can run faster than horses.",
+            "Biketour is fun!",
+            "Lots of people have cats as pets, but none are as cool as Elijah.",
+            "Tea is tastier than coffee",
+            "Halloween is soon",
+            "Cocoa nibs can be roasted and brewed like coffee"
+    };
+
+    //abilities
+    public String getFact() {
+
+        String fact = "";
+        //Randomly select a fact
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mFacts.length);
+
+        fact = mFacts[randomNumber];
+        return fact;
+    }
+}
--- app/src/main/java/treehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/treehouse/funfacts/FunFactsActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,46 +24,11 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] facts = {
-                    "Ants stretch when they wake up in the morning",
-                    "Ostriches can run faster than horses.",
-                    "Biketour is fun!"
-                };
-
-                //button was clicked, so update factlabel with a new fact
-                String fact = "";
-                //Randomly select a fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-
-                fact = facts[randomNumber];
-
+                String fact = mFactBook.getFact();
                 //Update the label with our dynamic fact
                 factLabel.setText(fact);
             }
         };
         showFactButton.setOnClickListener(listener);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
