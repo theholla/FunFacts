063ca8e4ce00926f3b8302f49210e52f21eed569
the holla <holla@bikefarm.org>
2015-10-18 22:50:30 -0700

    Add behavior for factButton

--- app/src/main/java/treehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/treehouse/funfacts/FunFactsActivity.java
@@ -1,9 +1,13 @@
 package treehouse.funfacts;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -11,6 +15,19 @@ public class FunFactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //declare our view variables and assign views from teh layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //button was clicked, so ui update factlabel with a new fact
+                String fact = "Ostriches can run faster than horses.";
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
     @Override
