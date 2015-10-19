48aae72d6b7ae7568c0da6b77e8219c895883450
the holla <holla@bikefarm.org>
2015-10-19 00:12:58 -0700

    Add behavior for changing background color with each click

--- app/src/main/java/treehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/treehouse/funfacts/FunFactsActivity.java
@@ -1,17 +1,20 @@
 package treehouse.funfacts;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,16 @@ public class FunFactsActivity extends AppCompatActivity {
         //declare our view variables and assign views from teh layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fact = mFactBook.getFact();
                 //Update the label with our dynamic fact
                 factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
